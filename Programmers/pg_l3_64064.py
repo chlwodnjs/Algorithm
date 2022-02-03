ee1d085d8d670e733d4cd3ccc3da57a27c4a22ee
chlwodnjs <wodnjs5171@gmail.com>
2022-02-03 21:26:54 +0900

    ban edit

--- Programmers/pg_l3_64064.py
+++ Programmers/pg_l3_64064.py
@@ -1,28 +1,22 @@
+from itertools import permutations
 import re
 
-def solution(user_id, banned_id):
-    answer = 0
-    blist=[]
-    dict_banned = {}
-    for i in banned_id:
-        dict_banned[i]=0
-        for j in user_id:
-            if len(i)==len(j):
-                check_i = "".join(re.findall("[a-zA-Z0-9]+", i))
-                check_i_len = len(check_i)
-                if check_i=="":
-                    dict_banned[i]=1
-                    user_id.remove(j)
-                    break
-                check_cnt = 0
-                for k in range(len(i)):
-                    if j[k]==i[k]:
-                        check_cnt+=1
-                if check_cnt==check_i_len:
-                    dict_banned[i]=1
-                    user_id.remove(j)
-                    break
 
-    for i in dict_banned.values():
-        answer+=i
-    return answer
+def solution(user_id, banned_id):
+    answer = ''
+    res = set(answer)
+    check_length = len(banned_id)
+    combination = list(permutations(user_id, check_length))
+    for comb in combination:
+        check = 0
+        for i in range(check_length):
+            if not re.match(banned_id[i].replace('*', '.'), comb[i]):
+                break
+            elif len(banned_id[i]) != len(comb[i]):
+                break
+            else:
+                check += 1
+        if check == check_length:
+            res.add(frozenset(comb))
+
+    return len(res)
