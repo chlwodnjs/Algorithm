e49e9663f7e9396aae2cf5ccaad5eebc132f343b
chlwodnjs <wodnjs5171@gmail.com>
2022-02-12 17:28:01 +0900

    NM

---
+++ Baekjoon/bk_bt_15664.py
@@ -0,0 +1,23 @@
+def bt(x):
+    if x==M:
+        if sorted(graph) == graph:
+            print(*graph)
+            return
+    flag=0
+    for i in range(N):
+        if not visit[i] and not flag==check[i]:
+            graph.append(check[i])
+            visit[i]=1
+            flag=check[i]
+            bt(x+1)
+            graph.pop()
+            visit[i]=0
+
+
+if __name__ =='__main__':
+    N,M = map(int,input().split())
+    check = list(map(int,input().split()))
+    check = sorted(check)
+    graph = []
+    visit =[0]*N
+    bt(0)
