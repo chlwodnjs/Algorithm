4d0595cbee0bb2fd4c352132a1f84f71ad366121
chlwodnjs <wodnjs5171@gmail.com>
2022-03-07 18:13:00 +0900

    edit

--- Programmers/pg_l2_12945.py
+++ Programmers/pg_l2_12945.py
@@ -3,6 +3,6 @@ def solution(n):
     check = [0]*(n+1)
     check[1]=1
     for i in range(2,n+1):
-        check[i] = (check[i-2]+check[i-1])%1234567
-    answer = check[n]
+        check[i] = check[i-2]+check[i-1]
+    answer = check[n]%1234567
     return answer
