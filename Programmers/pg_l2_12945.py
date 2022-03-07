091c838bd76c1794fc2c79abc3c5d46a94fe7f16
chlwodnjs <wodnjs5171@gmail.com>
2022-03-07 18:11:36 +0900

    fibo

---
+++ Programmers/pg_l2_12945.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = 0
+    check = [0]*(n+1)
+    check[1]=1
+    for i in range(2,n+1):
+        check[i] = (check[i-2]+check[i-1])%1234567
+    answer = check[n]
+    return answer
