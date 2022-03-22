4337db196bd7c2df38b46c80837750239ae4b2ca
chlwodnjs <wodnjs5171@gmail.com>
2022-03-22 20:48:04 +0900

    bt

--- Baekjoon/bk_bt_15684.py
+++ Baekjoon/bk_bt_15684.py
@@ -7,42 +7,38 @@ def check():
             elif s[j][temp - 1] == 1:
                 temp -= 1
         if temp != i:
-            return False
-    return True
+            return 0
+    return 1
 
 
 def dfs(num, cnt):
     global result
-    if result != 5:
+    if result < 5:
         return
     if num == cnt:
         if check():
             result = cnt
         return
-    for j in range(1, n):
-        for i in range(1, h + 1):
-            if s[i][j - 1] == 0 and s[i][j + 1] == 0 and s[i][j] == 0:
-                s[i][j] = 1
+    for bb in range(1, n):
+        for aa in range(1, h + 1):
+            if s[aa][bb - 1] == 0 and s[aa][bb + 1] == 0 and s[aa][bb] == 0:
+                s[aa][bb] = 1
                 dfs(num, cnt + 1)
-                s[i][j] = 0
-                while i < h:
-                    if s[i][j - 1] or s[i][j + 1]:
+                s[aa][bb] = 0
+                while aa < h:
+                    if s[aa][bb - 1] or s[aa][bb + 1]:
                         break
-                    i += 1
+                    aa += 1
 
 
 if __name__ == '__main__':
-    inf = 5
     n, m, h = map(int, input().split())
-    s = [[0] * (n + 1) for i in range(h + 1)]
-    result = inf
+    s = [[0] * (n + 1) for _ in range(h + 1)]
+    result = 5
     for i in range(m):
         a, b = map(int, input().split())
         s[a][b] = 1
     for i in range(4):
         dfs(i, 0)
-        if result != inf:
-            print(result)
-            break
-    if result == inf:
-        print(-1)
+
+    print(result if result < 5 else -1)
