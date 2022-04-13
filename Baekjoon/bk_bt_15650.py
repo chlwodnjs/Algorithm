190d51a9f98524b20c37e7b179ac95d49456c505
chlwodnjs <wodnjs5171@gmail.com>
2022-04-13 23:43:59 +0900

    nm2

--- Baekjoon/bk_bt_15650.py
+++ Baekjoon/bk_bt_15650.py
@@ -1,19 +1,19 @@
-def bt(m):
-    if m == M:
-        if sorted(graph) == graph:
-            print(*graph)
-        return
+def bt(x):
+    if x == M and sorted(check) == check:
+        for i in range(x):
+            print(' '.join(map(str, check)))
+            return
     for i in range(1, N + 1):
-        if not check[i]:
-            graph.append(i)
-            check[i] = 1
-            bt(m + 1)
-            graph.pop()
-            check[i] = 0
+        if not vis[i]:
+            check.append(i)
+            vis[i] = 1
+            bt(x + 1)
+            check.pop()
+            vis[i] = 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     N, M = map(int, input().split())
-    graph = []
-    check = [0] * 9
+    check = []
+    vis = [0] * 9
     bt(0)
